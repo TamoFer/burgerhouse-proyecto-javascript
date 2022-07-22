1d190c748163b1311d84aaac36f1f206df53776f
Fernando Tamone <104037683+FerTamone@users.noreply.github.com>
2022-07-22 03:29:10 -0300

    terminando fetch

--- hamburgueseria.js
+++ hamburgueseria.js
@@ -1,119 +1,127 @@
-// importacion de funciones de otros modulos
-// import {  datosCliente} from "./accionesBtns.js";
-// import { renderizarCards, mostrarCarrito } from "./renders.js";
+//importacion de funciones de otros modulos
+import {  datosCliente} from "./accionesBtns.js";
+import { renderizarCards, mostrarCarrito } from "./renders.js";
 
-function getDatos(){
-  const datos= [];
-  fetch('/bd.json')
-    .then(response => response.json())
-    .then(data => {
-      data.forEach(dato=>{
-        datos.push(dato);
-      })
+//traigo mi BD local y retorno una lista de objetos
+const objeto = ()=>{
+  const datos=[];
+  fetch("/bd.json")
+  .then(res=>res.json())
+  .then(data=>{
+    data.forEach(burgers=>{
+      datos.push(burgers);  
     })
+  })
   return datos;
+}
+
+//traigo mi BD local y renderizo las cards
+const objetoRender = ()=>{
+  fetch("/bd.json")
+  .then(res=>res.json())
+  .then(data=>{
+    renderizarCards(data);
+  })
+}
+
+//llamo a la funcion
+objetoRender();
+
+//instancio una constante con la lista de burgers de mi BD 
+export const BD=objeto();
+
+
+//definicion de constantes que utilizare en todo el codigo, agrego funcion export para modularizar el codigo
+export const listaPedidos = [];
+export const menus = document.querySelector("#menus");
+export const facturacion = document.querySelector("#facturacion");
+const montoTotal = document.querySelector("#montoTotal");
+
+
+//devuelve la suma total de todos los pedidos de mi lista de pedidos utilizando reduce
+export function sumaTotal(array) {
+  const total = array.reduce(
+    (acc, pedido) => acc + pedido.precio * pedido.cantidad,
+    0
+  );
+
+  montoTotal.innerHTML = `<h3>TOTAL $ ${total}</h3>`;
+  montoTotal.innerHTML += `
+              <button type="button" id="modal-btn" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">PAGAR TODO</button>
+              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Datos del cliente</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                  <form id="form" class="row g-3">
+                  
+                    <div class="col-md-4">
+                      <label  for="validationServer01" class="form-label">Nombre</label>
+                      <input id="nombre" type="text" class="form-control" id="validationServer01"  required>
+                    </div>
+
+                    <div class="col-md-4">
+                      <label  for="validationServer02" class="form-label">Apellido</label>
+                      <input id="apellido" type="text" class="form-control" id="validationServer02" required>
+                    </div>
+
+                    <div class="col-md-4">
+                      <label  for="validationServer01" class="form-label">Teléfono</label>
+                      <input id="telefono" type="number" class="form-control" id="validationServer01"  required>
+                    </div>
+                    
+                    <div class="col-md-6">
+                      <label  for="validationServer03" class="form-label">Dirección</label>
+                      <input id="direccion" type="text" class="form-control" id="validationServer03" aria-describedby="validationServer03Feedback" required>
+                    </div>
+
+                    <div class="col-md-3">
+                      <label for="validationServer05" class="form-label">C.P</label>
+                      <input type="text" class="form-control" id="validationServer05" aria-describedby="validationServer05Feedback" required>
+                    </div>
+
+                    <div class="col-md-6 mb-3">
+                    <label for="validationServer03" class="form-label">Forma de Pago</label>
+                      <select id="fpago" class="form-select" required aria-label="select example">
+                        <option value="">Selecciona </option>
+                        <option value="1">Efectivo</option>
+                        <option value="2">MercadoPago</option>
+                      </select>
+                    </div>
+
+                    <div class="col-md-6>
+                      <label for="validationServer03" class="form-label p-2">TOTAL</label>
+                      <input id="total" class="form-control-6 col-2" type="text" value="$${total}" aria-label="readonly input example" readonly>
+                    </div>
+
+                    <div class="col-12 d-md-flex p-1 justify-content-md-center">
+                      <button  class="btn btn-primary"  id="liveToastBtn" type="submit" data-bs-dismiss="modal" >Abonar el pedido</button>
+                    </div>
+                    
+                  </form>
+                  
+                </div>
+              </div>
+            </div>`;
+  datosCliente();
 };
 
-const objeto = getDatos();
-console.log(typeof objeto);
+// funcion de guardado en LocalStorage de mi lista de pedidos
+export function guardandoLocalmente(listaPedidos) {
+  localStorage.setItem("pedido", JSON.stringify(listaPedidos));
+}
 
-// objeto.forEach(p=>{
-//   console.log(p);
-// });
-
-// definicion de constantes que utilizare en todo el codigo, agrego funcion export para modularizar el codigo
-// export const listaPedidos = [];
-// export const menus = document.querySelector("#menus");
-// export const facturacion = document.querySelector("#facturacion");
-// const montoTotal = document.querySelector("#montoTotal");
-
-// //instancio funcion para ver cards por pantalla
-// renderizarCards(hamburguesas);
-
-// //devuelve la suma total de todos los pedidos de mi lista de pedidos utilizando reduce
-// export function sumaTotal(array) {
-//   const total = array.reduce(
-//     (acc, pedido) => acc + pedido.precio * pedido.cantidad,
-//     0
-//   );
-
-//   montoTotal.innerHTML = `<h3>TOTAL $ ${total}</h3>`;
-//   montoTotal.innerHTML += `
-//               <button type="button" id="modal-btn" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">PAGAR TODO</button>
-//               <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
-
-//               <div class="modal-dialog">
-//                 <div class="modal-content">
-//                   <div class="modal-header">
-//                     <h5 class="modal-title" id="exampleModalLabel">Datos del cliente</h5>
-//                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//                   </div>
-//                   <div class="modal-body">
-//                   <form id="form" class="row g-3">
-                  
-//                     <div class="col-md-4">
-//                       <label  for="validationServer01" class="form-label">Nombre</label>
-//                       <input id="nombre" type="text" class="form-control" id="validationServer01"  required>
-//                     </div>
-
-//                     <div class="col-md-4">
-//                       <label  for="validationServer02" class="form-label">Apellido</label>
-//                       <input id="apellido" type="text" class="form-control" id="validationServer02" required>
-//                     </div>
-
-//                     <div class="col-md-4">
-//                       <label  for="validationServer01" class="form-label">Teléfono</label>
-//                       <input id="telefono" type="number" class="form-control" id="validationServer01"  required>
-//                     </div>
-                    
-//                     <div class="col-md-6">
-//                       <label  for="validationServer03" class="form-label">Dirección</label>
-//                       <input id="direccion" type="text" class="form-control" id="validationServer03" aria-describedby="validationServer03Feedback" required>
-//                     </div>
-
-//                     <div class="col-md-3">
-//                       <label for="validationServer05" class="form-label">C.P</label>
-//                       <input type="text" class="form-control" id="validationServer05" aria-describedby="validationServer05Feedback" required>
-//                     </div>
-
-//                     <div class="col-md-6 mb-3">
-//                     <label for="validationServer03" class="form-label">Forma de Pago</label>
-//                       <select id="fpago" class="form-select" required aria-label="select example">
-//                         <option value="">Selecciona </option>
-//                         <option value="1">Efectivo</option>
-//                         <option value="2">MercadoPago</option>
-//                       </select>
-//                     </div>
-
-//                     <div class="col-md-6>
-//                       <label for="validationServer03" class="form-label p-2">TOTAL</label>
-//                       <input id="total" class="form-control-6 col-2" type="text" value="$${total}" aria-label="readonly input example" readonly>
-//                     </div>
-
-//                     <div class="col-12 d-md-flex p-1 justify-content-md-center">
-//                       <button  class="btn btn-primary"  id="liveToastBtn" type="submit" >Abonar el pedido</button>
-//                     </div>
-                    
-//                   </form>
-                  
-//                 </div>
-//               </div>
-//             </div>`;
-//   datosCliente();
-// };
-
-// // funcion de guardado en LocalStorage de mi lista de pedidos
-// export function guardandoLocalmente(listaPedidos) {
-//   localStorage.setItem("pedido", JSON.stringify(listaPedidos));
-// }
-
-// //evento DOMContentLoaded para que al recargar me muestre mi ultimo carrito creado
-// document.addEventListener("DOMContentLoaded", () => {
-//   if (localStorage.getItem("pedido")) {
-//     const pedidos = JSON.parse(localStorage.getItem("pedido"));
-//     pedidos.forEach((pedido) => {
-//       listaPedidos.push(pedido);
-//     });
-//     mostrarCarrito(listaPedidos);
-//   }
-// });
+//evento DOMContentLoaded para que al recargar me muestre mi ultimo carrito creado
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("pedido")) {
+    const pedidos = JSON.parse(localStorage.getItem("pedido"));
+    pedidos.forEach((pedido) => {
+      listaPedidos.push(pedido);
+    });
+    mostrarCarrito(listaPedidos);
+  }
+});
--- renders.js
+++ renders.js
@@ -1,61 +1,61 @@
-// importacion de funciones de otros modulos
-// import { agregarPedido,eliminarPedido} from "./accionesBtns.js";
-// import { menus,facturacion,guardandoLocalmente, sumaTotal} from "./hamburgueseria.js";
+//importacion de funciones de otros modulos
+import { agregarPedido,eliminarPedido} from "./accionesBtns.js";
+import { menus,facturacion,guardandoLocalmente, sumaTotal} from "./hamburgueseria.js";
 
-// //render de cards
-// export function renderizarCards(objeto) {
-//   objeto.forEach((menu) => {
-//     let {serial,nombre,carne,medallones,guarnicion,ingredientes,precio}=menu;
+//render de cards
+export function renderizarCards(objeto) {
+  objeto.forEach((menu) => {
+    let {serial,nombre,carne,medallones,guarnicion,ingredientes,precio}=menu;
 
-//     const miCard = document.createElement("div");
-//     miCard.classList.add("pedido");
+    const miCard = document.createElement("div");
+    miCard.classList.add("pedido");
 
-//     const miCardTitle = document.createElement("h2");
-//     miCardTitle.textContent = nombre;
+    const miCardTitle = document.createElement("h2");
+    miCardTitle.textContent = nombre;
 
-//     const miCardDescripcion = document.createElement("p");
-//     miCardDescripcion.textContent = `Deliciosa Hamburguesa de ${carne} ${medallones} que trae ${ingredientes} acompañado de ${guarnicion}`;
+    const miCardDescripcion = document.createElement("p");
+    miCardDescripcion.textContent = `Deliciosa Hamburguesa de ${carne} ${medallones} que trae ${ingredientes} acompañado de ${guarnicion}`;
 
-//     const miCardPrecio = document.createElement("h4");
-//     miCardPrecio.textContent = `Precio $ ${precio}`;
+    const miCardPrecio = document.createElement("h4");
+    miCardPrecio.textContent = `Precio $ ${precio}`;
 
-//     const tktBoton = document.createElement("button");
-//     tktBoton.classList.add("btn");
-//     tktBoton.classList.add("btn-info");
-//     tktBoton.textContent = "Agregar";
-//     tktBoton.setAttribute("serial", serial);
-//     tktBoton.addEventListener("click", agregarPedido);
+    const tktBoton = document.createElement("button");
+    tktBoton.classList.add("btn");
+    tktBoton.classList.add("btn-info");
+    tktBoton.textContent = "Agregar";
+    tktBoton.setAttribute("serial", serial);
+    tktBoton.addEventListener("click", agregarPedido);
 
-//     miCard.appendChild(miCardTitle);
-//     miCard.appendChild(miCardDescripcion);
-//     miCard.appendChild(miCardPrecio);
-//     miCard.appendChild(tktBoton);
-//     menus.appendChild(miCard);
-//   });
-// };
+    miCard.appendChild(miCardTitle);
+    miCard.appendChild(miCardDescripcion);
+    miCard.appendChild(miCardPrecio);
+    miCard.appendChild(tktBoton);
+    menus.appendChild(miCard);
+  });
+};
 
-// //render de carrito
-// export function mostrarCarrito(listaPedidos) {
-//   facturacion.textContent = "";
-//   listaPedidos.forEach((pedido) => {
-//     let {serial,nombre,precio,cantidad}= pedido;
-//     const miTkt = document.createElement("div");
-//     miTkt.classList.add("descripcionTkt");
+//render de carrito
+export function mostrarCarrito(listaPedidos) {
+  facturacion.textContent = "";
+  listaPedidos.forEach((pedido) => {
+    let {serial,nombre,precio,cantidad}= pedido;
+    const miTkt = document.createElement("div");
+    miTkt.classList.add("descripcionTkt");
     
-//     const miTktDescripcion = document.createElement("p");
-//     miTktDescripcion.textContent = `${cantidad} x Hamburguesa ${nombre} `;
+    const miTktDescripcion = document.createElement("p");
+    miTktDescripcion.textContent = `${cantidad} x Hamburguesa ${nombre} `;
 
-//     const tktBoton = document.createElement("button");
-//     tktBoton.classList.add("btn");
-//     tktBoton.classList.add("btn-danger");
-//     tktBoton.textContent = "Eliminar";
-//     tktBoton.setAttribute("serialDelete",serial);
-//     tktBoton.addEventListener("click", eliminarPedido);
+    const tktBoton = document.createElement("button");
+    tktBoton.classList.add("btn");
+    tktBoton.classList.add("btn-danger");
+    tktBoton.textContent = "Eliminar";
+    tktBoton.setAttribute("serialDelete",serial);
+    tktBoton.addEventListener("click", eliminarPedido);
 
-//     miTkt.appendChild(miTktDescripcion);
-//     miTkt.appendChild(tktBoton);
-//     facturacion.appendChild(miTkt);
-//   });
-//   guardandoLocalmente(listaPedidos);
-//   sumaTotal(listaPedidos);
-// }
+    miTkt.appendChild(miTktDescripcion);
+    miTkt.appendChild(tktBoton);
+    facturacion.appendChild(miTkt);
+  });
+  guardandoLocalmente(listaPedidos);
+  sumaTotal(listaPedidos);
+}
