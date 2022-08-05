60fef16a9e100bca7d648125a7609b3fc72423b2
Fernando Tamone <104037683+FerTamone@users.noreply.github.com>
2022-08-04 21:02:54 -0300

    en proceso aun

--- Js/hamburgueseria.js
+++ Js/hamburgueseria.js
@@ -1,44 +1,40 @@
 //importacion de funciones de otros modulos
-import { mostrarCarrito, renderizarCards} from "./renders.js";
+import { mostrarCarrito, renderizarCards } from "./renders.js";
 import { DateTime } from "./luxon.js";
 
 //traigo mi BD local y retorno una lista de objetos
-const objeto = ()=>{
-  const datos=[];
+const objeto = () => {
+  const datos = [];
   fetch("bd_local/bd.json")
-  .then(res=>res.json())
-  .then(data=>{
-    data.forEach(burgers=>{
-      datos.push(burgers);  
-    })
-  })
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((burgers) => {
+        datos.push(burgers);
+      });
+    });
   return datos;
-}
+};
 
 //traigo mi BD local y renderizo las cards
-const objetoRender = ()=>{
+const objetoRender = () => {
   fetch("bd_local/bd.json")
-  .then(res=>res.json())
-  .then(data=>{
-    renderizarCards(data);
-  })
-}
+    .then((res) => res.json())
+    .then((data) => {
+      renderizarCards(data);
+    });
+};
 
 //llamo a la funcion
 objetoRender();
 
-//instancio una constante con la lista de burgers de mi BD 
-export const BD=objeto();
-
+//instancio una constante con la lista de burgers de mi BD
+export const BD = objeto();
 
 //definicion de constantes que utilizare en todo el codigo, agrego funcion export para modularizar el codigo
 export const listaPedidos = [];
 export const menus = document.querySelector("#menus");
 export const carritoBody = document.querySelector("#carrito-body");
 const montoTotal = document.querySelector("#montoTotal");
-
-
-
 
 //devuelve la suma total de todos los pedidos de mi lista de pedidos utilizando reduce
 export function sumaTotal(array) {
@@ -48,15 +44,16 @@ export function sumaTotal(array) {
   );
   const totalMonto = document.querySelector("#monto-carrito-total");
   totalMonto.textContent = `$${total}`;
-  tiempoDemora(array)
+  tiempoLlegadaPedido(array);
+  // console.log(calculoDemora(listaPedidos));
+  console.log(tiempoLlegadaPedido(listaPedidos));
   return total;
-};
+}
 
 // funcion de guardado en LocalStorage de mi lista de pedidos
 export function pedidoTemporal(listaPedidos) {
   localStorage.setItem("pedidoTemporal", JSON.stringify(listaPedidos));
 }
-
 
 //evento DOMContentLoaded para que al recargar me muestre mi ultimo carrito creado
 document.addEventListener("DOMContentLoaded", () => {
@@ -67,52 +64,48 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     mostrarCarrito(listaPedidos);
     contador();
-  };
+  }
 });
 
-const btnCarrito = document.querySelector('#carritoImg');
+const btnCarrito = document.querySelector("#carritoImg");
 btnCarrito.addEventListener("click", () => {
   mostrarCarrito(listaPedidos);
   sumaTotal(listaPedidos);
 });
 
-
-export function contador(){
-  const notificacion= document.querySelector('.contador');
-  const total = listaPedidos.reduce(
-    (acc, pedido) => acc + pedido.cantidad,
-    0
-  );
+export function contador() {
+  const notificacion = document.querySelector(".contador");
+  const total = listaPedidos.reduce((acc, pedido) => acc + pedido.cantidad, 0);
   notificacion.textContent = total.toString();
 }
 
-class Cliente{
-  constructor (nombre, telefono, direccion, fpago) {
+class Cliente {
+  constructor(nombre, telefono, direccion, fpago) {
     this.nombre = nombre;
     this.telefono = telefono;
     this.direccion = direccion;
     this.fpago = fpago;
   }
-};
+}
 
 function resetInterface() {
   localStorage.removeItem("pedidoTemporal");
-  const listaPedidos= [];
+  const listaPedidos = [];
   mostrarCarrito(listaPedidos);
   contador();
   window.location.reload();
-};
+}
 
-const listaClientes=[];
+const listaClientes = [];
 
-
-const btnModal= document.querySelector('#modal-btn');
-btnModal.addEventListener('click', () =>{
+const btnModal = document.querySelector("#modal-btn");
+btnModal.addEventListener("click", () => {
   Swal.fire({
-    title: 'Datos de envio',
-    html: `<input type="text" id="nombre" class="swal2-input" placeholder="Nombre ...">
-    <input type="text" id="telefono" class="swal2-input" placeholder="Telefono...">
-    <input type="text" id="direccion" class="swal2-input" placeholder="Direccion...">
+    title: "Datos de envio",
+    html: `
+    <input type="text" id="name" class="swal2-input" placeholder="Nombre ...">
+    <input type="text" id="tel" class="swal2-input" placeholder="Telefono...">
+    <input type="text" id="adress" class="swal2-input" placeholder="Direccion...">
     <div class="menu-fpago">
       <p>TOTAL $${sumaTotal(listaPedidos)}</p>
       <p>¿Como pagás?</p>
@@ -123,58 +116,59 @@ btnModal.addEventListener('click', () =>{
     </div>
     `,
     showConfirmButton: true,
-    confirmButtonText: 'Finalizar pedido',
+    confirmButtonText: "Finalizar pedido",
     focusConfirm: false,
     preConfirm: () => {
-      const nombre = Swal.getPopup().querySelector('#nombre').value;
-      const telefono = Swal.getPopup().querySelector('#telefono').value;
-      const direccion= Swal.getPopup().querySelector('#direccion').value;
-      const fpago= Swal.getPopup().querySelector('#fpago').value;
-      const cliente= new Cliente(nombre,telefono,direccion,fpago);
-      if (!nombre || !telefono || !direccion || !fpago ) {
-        Swal.showValidationMessage(`¡Completa todos los campos del formulario para el envio!`);
+      const nombre = Swal.getPopup().querySelector("#name").value;
+      const telefono = Swal.getPopup().querySelector("#tel").value;
+      const direccion = Swal.getPopup().querySelector("#adress").value;
+      const fpago = Swal.getPopup().querySelector("#fpago").value;
+      const cliente = new Cliente(nombre, telefono, direccion, fpago);
+      if (!nombre || !telefono || !direccion || !fpago) {
+        Swal.showValidationMessage(
+          `¡Completa todos los campos del formulario para el envio!`
+        );
       }
-      return cliente ;
-    }
+      return cliente;
+    },
   }).then((result) => {
-      const cliente= result.value;
-      listaClientes.push(cliente);
-      console.log(listaClientes);
-      Swal.fire(
-        {
-          icon: 'success',
-          text: `Excelente ${result.value.nombre} tu pedido ya esta pago llegara aprox. en 45min a ${result.value.direccion}. ¡Gracias por comprar en BurgerHouse!`,
-          timer: 3000
-          
-        }
-        
-      )
-      setTimeout(resetInterface, 4000);
-      
-  })
-  
+    const cliente = result.value;
+    listaClientes.push(cliente);
+    Swal.fire({
+      icon: "success",
+      text: `Excelente ${result.value.nombre} ${calculoDemora(listaPedidos)} a ${result.value.direccion}. ¡Gracias por comprar en BurgerHouse!`,
+      timer: 3000,
+    });
+    setTimeout(resetInterface, 4000);
+  });
 });
 
-const verPedido= document.querySelector('#ultimo-pedido');
-verPedido.addEventListener('click', () => {
+const verPedido = document.querySelector("#ultimo-pedido");
+verPedido.addEventListener("click", () => {
   console.log(listaClientes);
 });
 
+function tiempoLlegadaPedido(array) {
+  const numBurgers = array.reduce((acc, p) => acc + p.cantidad, 0);
+  const hora = DateTime.now();
+  const horaLlegada = hora
+    .plus({ hour: numBurgers * 0.25 })
+    .toLocaleString(DateTime.TIME_24_SIMPLE);
+  return horaLlegada;
+}
 
-function tiempoDemora(array){
-  const numBurgers = array.reduce(
-    (acc, p) => acc + p.cantidad,
-    0
-  );
-  const result= numBurgers*0.25;
-  const hora=DateTime.now();
-  const horaLllegada= hora.plus({hours:result});
-  console.log(horaLllegada.toLocaleString(DateTime.TIME_SIMPLE));
-};
+function calculoDemora(array) {
+  const numBurgers = array.reduce((acc, p) => acc + p.cantidad, 0);
+  const segundos = numBurgers * 900;
+  let hora = Math.floor(segundos / 3600);
+  hora = hora < 10 ? "0" + hora : hora;
+  let minutos = Math.floor((segundos / 60) % 60);
+  minutos = minutos < 10 ? "0" + minutos : minutos;
+  let demora = "";
 
+  hora < 1
+    ? (demora = `Tu pedido tardara aprox. ${minutos} min`)
+    : (demora = `Tu pedido tardara aprox. ${hora}:${minutos} hs`);
 
-const dt=DateTime.now().toLocaleString(DateTime.TIME_SIMPLE);
-
-// console.log(dt.plus({hour: 0.5}).hour);
-
-
+  return demora;
+}
